3f9123387ff54c3f254f3c5e98e04f84e7158bf5
danyGoncalvesNovais <91186956+danyGoncalvesNovais@users.noreply.github.com>
2022-11-10 17:01:04 +0100

    toot

--- Swift_Knowledge/php/affichageFiche.php
+++ Swift_Knowledge/php/affichageFiche.php
@@ -1,6 +1,8 @@
 <?php
 include "../Model/FicheModel.php";
 
+$fiche= new FicheModel;
+$reponse=$fiche->trouveUneFiche($titre, $Fiche,$categorie);
 
 $template = "../View/afficheFiche.phtml";
 require "../View/layout.phtml";
